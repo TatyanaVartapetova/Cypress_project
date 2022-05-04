1cfe33e877c908744a6e7c8a9b39de70674d850e
Vartapetova Tatyana <vartapetova@mybook.com>
2022-05-04 12:21:40 +0300

    delete all code for master

--- cypress/integration/mytests/my1test.spec.js
+++ cypress/integration/mytests/my1test.spec.js
@@ -1,9 +1,1 @@
 /// <reference types="Cypress" />
-it.only('using get with find and eq', () => {
-    cy.viewport(1800, 700)
-    cy.visit('https://docs.cypress.io/api/commands/eq')
-cy.get('div').find('nav').find('[href="#Syntax"]')
-});
-
-//comment
-//comment2
