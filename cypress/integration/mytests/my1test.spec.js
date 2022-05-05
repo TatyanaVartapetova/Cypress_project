3367d1a4990ed5d67f1e5a88646332acea441300
Vartapetova Tatyana <vartapetova@mybook.com>
2022-05-05 15:20:51 +0300

    get with find and eq

--- cypress/integration/mytests/my1test.spec.js
+++ cypress/integration/mytests/my1test.spec.js
@@ -1,1 +1,11 @@
 /// <reference types="Cypress" />
+
+it('using get with find and eq', ()=> {
+    cy.visit('https://kuchaknig.org/fotoalbums/')
+    cy.get(".wrap").find(".album_cover").find(".comment").eq(3)
+})
+
+it.only('using get with find and eq', ()=> {
+    cy.visit('https://kuchaknig.org/')
+    cy.get("#left_navigation").find("ul").find("li").find("a").eq(0)
+})
