742a34f32fe6db9adfa36a1b9dc0ce1e77f578a2
Vartapetova Tatyana <vartapetova@mybook.com>
2022-05-05 12:54:29 +0300

    3.14.get

--- cypress/integration/mytests/my1test.spec.js
+++ cypress/integration/mytests/my1test.spec.js
@@ -1,6 +1,44 @@
 /// <reference types="Cypress" />
-it.only('using get with find and eq', () => {
-    cy.viewport(1800, 700)
-    cy.visit('https://docs.cypress.io/api/commands/eq')
-cy.get('div').find('nav').find('[href="#Syntax"]')
-});
+/// <reference types="Cypress" />
+
+//const { should } = require("chai")
+
+it('by id', ()=> {
+    cy.visit('https://www.wildberries.ru/')
+    cy.get("#searchInput")
+})
+
+it('by class', ()=> {
+    cy.visit('https://www.wildberries.ru/')
+    cy.get(".search-catalog__block")
+})
+
+it('by tag', ()=> {
+    cy.visit('https://www.ozon.ru/')
+    cy.get("ul")
+})
+
+it('by attribute', ()=> {
+    cy.visit('https://www.wildberries.ru/')
+    cy.get('[data-wba-header-name="Country"]')
+})
+
+it('by different attribute', ()=> {
+    cy.visit('https://www.wildberries.ru/')
+    cy.get('[type="button"][aria-label="Онлайн чат"]')
+})
+
+it('by different types', ()=> {
+    cy.visit('https://www.wildberries.ru/')
+    cy.get('[type="button"][aria-label="Онлайн чат"]')
+})
+
+it('by tag and attribute', ()=> {
+    cy.visit('https://www.wildberries.ru/')
+    cy.get('button[aria-label="Навигация по сайту"]')
+})
+
+it.only('by contains name', ()=> {
+    cy.visit('https://www.wildberries.ru/')
+    cy.get('*[href^="/services"]') // class name start with goods for all (*) selectors
+})
