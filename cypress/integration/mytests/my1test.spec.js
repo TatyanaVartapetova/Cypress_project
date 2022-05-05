763e483df135d2d568bb62cfd144e5aa3ba9ae66
Vartapetova Tatyana <vartapetova@mybook.com>
2022-05-05 11:03:42 +0300

    commit

--- cypress/integration/mytests/my1test.spec.js
+++ cypress/integration/mytests/my1test.spec.js
@@ -1,1 +1,17 @@
 /// <reference types="Cypress" />
+
+//const { should } = require("chai")
+
+it('should', ()=> {
+    cy.visit('https://www.wildberries.ru/')
+    cy.get("[id='searchInput']")
+    .type('neva')
+    .should('have.value', 'neva') //если цифры то без кавычек и тут и выше
+    .and('be.visible')
+})
+
+
+it.only('should', ()=> {
+    cy.visit('https://www.ozon.ru/highlight/ozon-retail-186820/?delivery=1')
+    cy.get('[data-widget="searchResultsFilters"]')
+})
